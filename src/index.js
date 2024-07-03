fdf90b49ebd9bb10fd7727f47d9dc68abd42d84c
Sarthak_Kotia <sarthakkotia@gmail.com>
2024-07-03 21:59:16 +0530

    mongodb atlas connected

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path: './env'}) // decreases consistencty
+// improved
+import dotenv from"dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express()
+// function connectDB(){}
+
+// connectDB()
+
+
+
+
+//using IIFE
+;(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ",error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening in port ${process.env.PORT}`)
+        })
+
+    }catch(error){
+        console.error("ERROR: ",error)
+        throw error
+    }
+})()
+
+*/
